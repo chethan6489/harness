78c0ae5021e307bb387154c154a51633e1675afa
chethan6489 <65522827+chethan6489@users.noreply.github.com>
2024-10-16 09:38:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.0"
+    }    
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
+resource "azurerm_resource_group" "wells" {
+  name     = "wellsfargo"
+  location = "West Europe"
+}
